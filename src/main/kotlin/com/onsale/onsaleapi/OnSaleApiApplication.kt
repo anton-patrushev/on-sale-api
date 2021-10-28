76cb84f2b892bcf100dd1b954fd8bfc7a8519dc7
anton-patrushev <anton11patrushev@gmail.com>
2021-10-28 11:57:20 +0300

    initial commit

---
+++ src/main/kotlin/com/onsale/onsaleapi/OnSaleApiApplication.kt
@@ -0,0 +1,11 @@
+package com.onsale.onsaleapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnSaleApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnSaleApiApplication>(*args)
+}

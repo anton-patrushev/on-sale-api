18c9be385841d416488485c4246dc9aa42a26647
anton-patrushev <anton11patrushev@gmail.com>
2021-11-17 10:14:06 +0300

    Install Offer entities and DB table

---
+++ src/main/kotlin/com/onsale/onsaleapi/domains/offers/db/OffersTable.kt
@@ -0,0 +1,20 @@
+package com.onsale.onsaleapi.domains.offers.db
+
+import com.onsale.onsaleapi.domains.cities.db.CitiesTable
+import com.onsale.onsaleapi.domains.companies.db.CompaniesTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.date
+import java.time.LocalDate
+import java.util.*
+
+object OffersTable : Table("Offers") {
+    val id: Column<UUID> = uuid("id").uniqueIndex()
+    val companyId: Column<UUID> = reference("company_id", CompaniesTable.id)
+    val description: Column<String?> = varchar("description", 50).nullable()
+    val sale: Column<String?> = varchar("sale", 50).nullable()
+    val cityId: Column<UUID> = reference("city_id", CitiesTable.id)
+    val createdAt: Column<LocalDate> = date("created_at") // TODO: Date type
+
+    override val primaryKey: PrimaryKey = PrimaryKey(id)
+}
---
+++ src/main/kotlin/com/onsale/onsaleapi/domains/offers/entities/Offer.kt
@@ -0,0 +1,35 @@
+package com.onsale.onsaleapi.domains.offers.entities
+
+import com.onsale.onsaleapi.domains.shared.types.ID
+
+//data class OfferCompanyInfo(
+//        val id: ID,
+//        val name: String,
+//        val websiteURL: String?,
+//        val phone: String?,
+//        val workingSchedule: String?
+//)
+//
+//data class OfferCityInfo(
+//        val id: ID,
+//        val name: String
+//)
+
+data class Offer(
+        val id: ID,
+        val companyId: ID,
+        val description: String?,
+        val sale: String?,
+        val cityId: ID,
+        val created_at: String, // TODO: Date type
+) {
+    companion object {}
+}
+
+data class OfferFields(
+        val companyId: ID?,
+        val description: String?,
+        val sale: String?,
+        val cityId: ID?,
+        val created_at: String?, // TODO: Date type
+)

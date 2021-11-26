400b601f033bf714800ddfc06cfd49e77f27efd0
anton-patrushev <anton11patrushev@gmail.com>
2021-11-27 02:43:54 +0300

    Fix null casting into String

--- src/main/kotlin/com/onsale/onsaleapi/domains/companies/entities/Company.kt
+++ src/main/kotlin/com/onsale/onsaleapi/domains/companies/entities/Company.kt
@@ -5,32 +5,32 @@ import com.onsale.onsaleapi.domains.shared.types.ID
 import org.jetbrains.exposed.sql.ResultRow
 
 data class Company(
-        val id: ID,
-        val name: String,
-        val websiteURL: String?,
-        val email: String?,
-        val phone: String?,
-        val workSchedule: String?
+    val id: ID,
+    val name: String,
+    val websiteURL: String?,
+    val email: String?,
+    val phone: String?,
+    val workSchedule: String?
 ) {
     companion object {}
 }
 
 data class CompanyFields(
-        val name: String?,
-        val websiteURL: String?,
-        val email: String?,
-        val phone: String?,
-        val workSchedule: String?
+    val name: String?,
+    val websiteURL: String?,
+    val email: String?,
+    val phone: String?,
+    val workSchedule: String?
 )
 
 // TODO: move into better place
 fun Company.Companion.fromDBRow(row: ResultRow): Company {
     return Company(
-            row[CompaniesTable.id].toString(),
-            row[CompaniesTable.name],
-            row[CompaniesTable.websiteURL].toString(),
-            row[CompaniesTable.email].toString(),
-            row[CompaniesTable.phone].toString(),
-            row[CompaniesTable.workSchedule].toString()
+        row[CompaniesTable.id].toString(),
+        row[CompaniesTable.name],
+        row[CompaniesTable.websiteURL]?.toString(),
+        row[CompaniesTable.email]?.toString(),
+        row[CompaniesTable.phone]?.toString(),
+        row[CompaniesTable.workSchedule]?.toString()
     )
 }

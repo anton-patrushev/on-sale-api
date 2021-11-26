bba47996f2843860640edd015b7d34e30e4c9d37
anton-patrushev <anton11patrushev@gmail.com>
2021-11-26 22:09:39 +0300

    drop duplicated offer service

--- src/main/kotlin/com/onsale/onsaleapi/domains/offers/controllers/OfferController.kt
+++ src/main/kotlin/com/onsale/onsaleapi/domains/offers/controllers/OfferController.kt
@@ -1,13 +1,9 @@
 package com.onsale.onsaleapi.domains.offers.controllers
 
-import com.onsale.onsaleapi.domains.cities.services.ICityService
-import com.onsale.onsaleapi.domains.companies.services.ICompanyService
 import com.onsale.onsaleapi.domains.offers.controllers.OfferController.Companion.BASE_OFFER_URL
 import com.onsale.onsaleapi.domains.offers.dto.*
 import com.onsale.onsaleapi.domains.offers.entities.OfferJoined
 import com.onsale.onsaleapi.domains.offers.mappers.*
-import com.onsale.onsaleapi.domains.offers.mappers.common.OfferResponseMapperAdditionalParams
-import com.onsale.onsaleapi.domains.offers.services.IOfferService
 import com.onsale.onsaleapi.domains.offers.services.OfferJoinedService
 import com.onsale.onsaleapi.domains.shared.types.ID
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,11 +14,8 @@ import java.net.URI
 @RestController
 @RequestMapping(value = [BASE_OFFER_URL])
 class OfferController(
-    @Autowired val offerService: IOfferService,
     @Autowired val offerJoinedService: OfferJoinedService,
-    @Autowired val companyService: ICompanyService,
-    @Autowired val cityService: ICityService,
-    @Autowired val getByIdOfferResponseMapper: GetByIdOfferResponseMapper,
+    @Autowired val getByIdOfferResponseMapper: GetByIdJoinedOfferResponseMapper,
     @Autowired val getJoinedOffersResponseMapper: GetJoinedOffersResponseMapper,
     @Autowired val createJoinedOfferResponseMapper: CreateJoinedOfferResponseMapper,
     @Autowired val updateJoinedOfferResponseMapper: UpdateJoinedOfferResponseMapper,
@@ -35,13 +28,10 @@ class OfferController(
 
     @GetMapping("/{id}")
     override fun getOfferById(@PathVariable id: ID): ResponseEntity<GetByIdOfferResponse> {
-        val offer = offerService.getById(id) ?: return ResponseEntity.notFound().build()
-
-        val company = companyService.getById(offer.companyId) ?: return ResponseEntity.internalServerError().build()
-        val city = cityService.getById(offer.cityId) ?: return ResponseEntity.internalServerError().build()
+        val offer = offerJoinedService.getById(id) ?: return ResponseEntity.notFound().build()
 
         return ResponseEntity.ok(
-            getByIdOfferResponseMapper.transform(offer, OfferResponseMapperAdditionalParams(company, city))
+            getByIdOfferResponseMapper.transform(offer)
         )
     }
 
@@ -69,7 +59,7 @@ class OfferController(
     ): ResponseEntity<UpdateOfferResponse> {
         val offer = offerJoinedService.edit(id, request)
 
-        return when(offer) {
+        return when (offer) {
             is OfferJoined -> ResponseEntity.ok(updateJoinedOfferResponseMapper.transform(offer))
             else -> ResponseEntity.notFound().build()
         }
@@ -79,7 +69,7 @@ class OfferController(
     override fun deleteOffer(@PathVariable id: ID): ResponseEntity<DeleteOfferResponse> {
         val offer = offerJoinedService.deleteById(id)
 
-        return when(offer) {
+        return when (offer) {
             is OfferJoined -> ResponseEntity.ok(deleteJoinedOfferResponseMapper.transform(offer))
             else -> ResponseEntity.notFound().build()
         }
